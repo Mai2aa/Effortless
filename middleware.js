ceca8fb19a71ffa9c5de7425f846608aa2b26d5f
Mai2aa <mysaadil0@gmail.com>
2025-01-01 20:14:59 +0300

    Basic Authentication - Public Route (Sign-in, Sign-up)

---
+++ middleware.js
@@ -0,0 +1,24 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// Define public routes
+const isPublicRoute = createRouteMatcher(['/sign-up(.*)', '/sign-in(.*)', '/']);
+
+export default clerkMiddleware(async (auth, request) => {
+  const pathname = request.nextUrl.pathname;
+
+  // Allow public routes to bypass authentication
+  if (isPublicRoute(request)) {
+    console.log('Public route accessed:', pathname);
+    return;
+  }
+
+  console.log('Protected route accessed:', pathname);
+  await auth.protect();
+});
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
